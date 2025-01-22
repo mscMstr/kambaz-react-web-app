6fbd4a848ff81dce83356df23643615172270277
mscMstr <delgadocarla2021@gmail.com>
2025-01-22 18:55:07 -0500

    added links to all the labs

---
+++ src/Labs/Lab6/index.tsx
@@ -0,0 +1,8 @@
+export default function Lab6() {
+    return (
+      <div id="wd-lab6">
+        <h2>Lab 6</h2>
+      </div>
+  );}
+  
+  
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,11 @@ export default function TOC() {
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+        <li><Link to="/Labs/Lab4">Lab 4</Link></li>
+        <li><Link to="/Labs/Lab5">Lab 5</Link></li>
+        <li><Link to="/Labs/Lab6">Lab 6</Link></li>
         <li><Link to="/Kambaz">Kambaz</Link></li>
+        <li><Link id="wd-github" to="https://github.com/mscMstr/kambaz-react-web-app">Github</Link></li>
       </ul>
     );
   }
