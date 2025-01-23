a897aad16c63b7daf3eceb68f35696b07cfd3ea0
mscMstr <delgadocarla2021@gmail.com>
2025-01-22 19:45:50 -0500

    account navigation and course module information

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -13,10 +13,42 @@ export default function Modules() {
                 <li className="wd-content-item">Learn what is Web Development</li>
               </ul>
             </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+                <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User Interfaces With HTML</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to Web Development</li>
+                <li className="wd-content-item">Creating an HTTP Server with Node.js</li>
+                <li className="wd-content-item">Creating a React Application</li>
+              </ul>
+            </li>
           </ul>
         </li>
         <li className="wd-module">
           <div className="wd-title">Week 2</div>
+          <ul className="wd-lessons">
+          <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+                <li className="wd-content-item">Deploy the assignment to Netlify</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to HTML and the DOM</li>
+                <li className="wd-content-item">Formatting Web Content with Headings and Paragraphs</li>
+                <li className="wd-content-item">Formatting Content with Lists and Tables</li>
+              </ul>
+            </li>
+          </ul>
         </li>
         <li className="wd-module">
           <div className="wd-title">Week 3</div>
