c8a532531a123d0ff12ab60f3f8c0cc5508a9b76
mscMstr <delgadocarla2021@gmail.com>
2025-02-03 18:55:39 -0500

    lab 1 exercies

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -31,6 +31,7 @@ tag to tell browsers to render the gaps.
 <div id="wd-lists">
 <h5>Ordered List Tag</h5>
   How to make pancakes:
+  <ol id="wd-pancakes">
     <li>Mix dry ingredients.</li>
     <li>Add wet ingredients.</li>
     <li>Stir to combine.</li>
@@ -39,6 +40,171 @@ tag to tell browsers to render the gaps.
     <li>Cook until bubbly on top.</li>
     <li>Flip and cook the other side.</li>
     <li>Serve and enjoy!</li>
+  </ol>
+    My favorite recipe:
+<ol id="wd-your-favorite-recipe">
+ <li>Add water to a pot and let boil.</li>
+ <li>Sprinkle salt into water.</li>
+ <li>When water is boiling add macaroni.</li>
+ <li>After about 18-22 minutes remove water from heat.</li>
+ <li>Strain pasta and pour into bowl.</li>
+ <li>Top with lots of cheese.</li>
+ </ol>
+ <h5>Unordered List Tag</h5>
+ My favorite books (in no particular order)
+ <ul id="wd-my-books">
+ <li>Dune</li>
+ <li>Lord of the Rings</li>
+ <li>Ender's Game</li>
+ <li>Red Mars</li>
+ <li>The Forever War</li>
+ </ul>
+ Your favorite books (in no particular order)
+ <ul id="wd-your-books">
+<li>It's Kind of a Funny Story</li>
+<li>Book Lovers</li>
+<li>Siz of Crows</li>
+<li>The Half-Blood Prince</li>
+<li>The Book Thief</li>
+ </ul>
+</div>
+
+<div id="wd-tables">
+  <h4>Table Tag</h4>
+ <table border={1} width="100%">
+<thead>
+  <tr>
+ <th>Quiz</th>
+ <th>Topic</th>
+ <th>Date</th>
+ <th>Grade</th>
+ </tr>
+ </thead>
+ <tbody>
+ <tr>
+ <td>Q1</td>
+ <td>HTML</td>
+ <td>2/3/21</td>
+ <td>85</td>
+ </tr>
+ <tr>
+<td>Q2</td>
+<tr> ... </tr>
+ <td>CSS</td>
+ <td>2/10/21</td>
+ <td>90</td>
+ </tr>
+ </tbody>
+ <tfoot>
+ <tr>
+ <td colSpan={3}>Average</td>
+ <td>90</td>
+ </tr>
+ </tfoot>
+ </table>
+ </div>
+
+ <div id="wd-images">
+  <h4>Image tag</h4>
+  Loading an image from the internet: <br />
+  <img id="wd-starship" width="400px"
+src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1
+_Starship-gap2.jpg" />
+  <br />
+  Loading a local image:
+  <br />
+  <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+</div>
+
+<div id="wd-forms">
+  <h4>Form Elements</h4>
+  <form id="wd-text-fields">
+    <h5>Text Fields</h5>
+    <label htmlFor="wd-text-fields-username">Username:</label>
+    <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+    <label htmlFor="wd-text-fields-password">Password:</label>
+    <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+    <br />
+    <label htmlFor="wd-text-fields-first-name">First name:</label>
+    <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+    <input type="text" placeholder="Doe"
+           value="Wonderland"
+           title="The last name"
+           id="wd-text-fields-last-name" />
+    <h5>Text boxes</h5>
+<label>Biography:</label><br/>
+<textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit
+amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum.</textarea>
+<h5 id="wd-radio-buttons">Radio buttons</h5>
+<label>Favorite movie genre:</label><br />
+<input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+<label htmlFor="wd-radio-comedy">Comedy</label><br />
+<input type="radio" name="radio-genre" id="wd-radio-drama"/>
+<label htmlFor="wd-radio-drama">Drama</label><br />
+<input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+<label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+<input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+<label htmlFor="wd-radio-fantasy">Fantasy</label>
+<h5 id="wd-checkboxes">Checkboxes</h5>
+<label>Favorite movie genre:</label><br/>
+<input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+<label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+<input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+<label htmlFor="wd-chkbox-drama">Drama</label><br/>
+<input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+<label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+<input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+<label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+<h4 id="wd-dropdowns">Dropdowns</h4>
+<h5>Select one</h5>
+<label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+<select id="wd-select-one-genre">
+   <option value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+   Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+<h5>Select many</h5>
+<label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+<select multiple id="wd-select-many-genre">
+   <option value="COMEDY" selected> Comedy          </option>
+   <option value="DRAMA">           Drama           </option>
+   <option value="SCIFI"  selected> Science Fiction </option>
+   <option value="FANTASY">         Fantasy         </option>
+</select>
+<h4>Other HTML field types</h4>
+<label htmlFor="wd-text-fields-email"> Email: </label>
+<input type="email"
+       placeholder="jdoe@somewhere.com"
+       id="wd-text-fields-email"/><br/>
+<label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+<input type="number"
+       value="100000"
+       placeholder="1000"
+       id="wd-text-fields-salary-start"/><br/>
+<label htmlFor="wd-text-fields-rating"> Rating: </label>
+<input type="range"
+       value="4"
+       max="5"
+       placeholder="Doe"
+       id="wd-text-fields-rating"/><br/>
+<label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+<input type="date"
+       value="2000-01-21"
+       id="wd-text-fields-dob"/><br/>
+  </form>
+  <h4>Anchor tag</h4>
+Please
+<a href="https://www.lipsum.com" id="wd-lipsum">click here</a>
+to get dummy text<br/>
 </div>
 </div>
 );}
