3281d2ac6227fc50dabbfb8f5f81d2a91f858784
mscMstr <delgadocarla2021@gmail.com>
2025-02-05 23:37:10 -0500

    search bar in line with buttons

--- src/Kambaz/Courses/Assignments/AssignmentControls.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentControls.tsx
@@ -3,18 +3,22 @@ import { Button, FormControl, InputGroup } from "react-bootstrap";
 import { IoIosSearch } from "react-icons/io";
 export default function AssignmentControls() {
  return (
-    <div className="text-nowrap ">
-     <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-assignment">
-       <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-       Assignment
-     </Button>
-     <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-assignment-group">
-       <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-       Group
-     </Button>
-     <InputGroup id="wd-search-assignment" className="mb-3">
-        <InputGroup.Text><IoIosSearch /></InputGroup.Text>
-        <FormControl placeholder="Search for Assignments" />
-     </InputGroup>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="flex-grow-1">
+        <InputGroup id="wd-search-assignment" className="mb-3">
+            <InputGroup.Text><IoIosSearch /></InputGroup.Text>
+            <FormControl placeholder="Search..." />
+        </InputGroup>
+        </div>
+        <div className="mx-4">
+            <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-assignment">
+                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                Assignment
+            </Button>
+            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-add-assignment-group">
+                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                Group
+            </Button>
+        </div>
    </div>
 );}
