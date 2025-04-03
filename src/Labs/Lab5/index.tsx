923235d66772178b9832df4d96994f17353ff1b5
mscMstr <delgadocarla2021@gmail.com>
2025-04-02 23:37:05 -0400

    commented out asynchronous arrays

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -4,7 +4,7 @@ import Module from "./Module";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
-import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+// import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
@@ -27,7 +27,7 @@ export default function Lab5() {
       <WorkingWithArrays />
       <HttpClient />
       <WorkingWithObjectsAsynchronously />
-      <WorkingWithArraysAsynchronously />
+      {/* <WorkingWithArraysAsynchronously /> */}
     </div>
   );
 }
